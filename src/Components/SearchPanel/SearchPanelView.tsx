338e64ed3273338e99495e26f718e569fbb038f3
anna.tsukanova <anna.tsukanova@kyivstar.net>
2020-04-17 21:06:55 +0300

    Added style for tablet and mobile

--- src/Components/SearchPanel/SearchPanelView.tsx
+++ src/Components/SearchPanel/SearchPanelView.tsx
@@ -3,7 +3,43 @@ import './SearchPanelStyle.scss'
 
 const SearchPanelView: React.FC = () => {
   return (
-    <div>
+    <div className="SearchPanel">
+      <div className="SearchPanel__item">
+        <input className="SearchPanel__input" type="radio" id="random" name="filters" value="random" />
+        <label htmlFor="random">Random</label>
+      </div>
+      <div className="SearchPanel__item">
+        <input className="SearchPanel__input" type="radio" id="categories" name="filters" value="categories" />
+        <label htmlFor="categories">From categories</label>
+        <ul className="SearchPanel__categories">
+          <li>
+            <input className="SearchPanel__checkbox" id="animal" type="checkbox" value="animal" />
+            <label htmlFor="animal">animal</label>
+          </li>
+          <li>
+            <input className="SearchPanel__checkbox" id="career" type="checkbox" value="career" checked />
+            <label htmlFor="career">career</label>
+          </li>
+          <li>
+            <input className="SearchPanel__checkbox" id="celebrity" type="checkbox" value="celebrity" />
+            <label htmlFor="celebrity">celebrity</label>
+          </li>
+          <li>
+            <input className="SearchPanel__checkbox" id="dev" type="checkbox" value="dev" />
+            <label htmlFor="dev">dev</label>
+          </li>
+        </ul>
+      </div>
+      <div className="SearchPanel__item">
+        <input className="SearchPanel__input" type="radio" id="search" name="filters" value="search" />
+        <label htmlFor="search">Search</label>
+        <input className="SearchPanel__search" type="search" id="SiteSearch" name="search"
+          placeholder="Free text search..."
+          aria-label="Search through jokes" />
+      </div>
+      <button className="SearchPanel__button" >
+        <span>Get a joke</span>
+      </button>
     </div>
   )
 }
